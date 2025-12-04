cd5943104122af7237943cc9ff9e1f0d3f479dce
Archgamer <zacharyarchlon@gmail.com>
2025-12-03 19:20:31 -0500

    Update SportCar.java

--- RentalProject/src/SportCar.java
+++ RentalProject/src/SportCar.java
@@ -6,9 +6,22 @@ public final class SportCar extends Car {
     public SportCar(String licensePlate, String make, String model, int year,
                     int horsepower, boolean hasTurbo) {
         super(licensePlate, make, model, year);
+        this.horsepower = horsepower;
+        this.hasTurbo = hasTurbo;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public boolean hasTurbo() {
+        return hasTurbo;
     }
 
     public String getInfo() {
-        return "";
+        return String.format(
+            "| %s\t| %s\t| %s\t| %d\t| %s\t| %d HP, Turbo: %b |",
+            getLicensePlate(), getMake(), getModel(), getYear(), getStatus(), horsepower, hasTurbo
+        );
     }
 }
